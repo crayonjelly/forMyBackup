f189de56c6e061691dc4b7d817551069c6cf46b1
Jericho <dldydwp76@gmail.com>
2017-11-04 18:57:37 +0900

    이미지 추가

--- owlboy/playGround.cpp
+++ owlboy/playGround.cpp
@@ -10,6 +10,15 @@ HRESULT playGround::init(void)
 	IMAGEMANAGER->addImage("cloudBack", "resource/(x2)cloudBack(1180,378).bmp", 1180, 378, true, RGB(255, 0, 255));
 	IMAGEMANAGER->addImage("bomboShop", "resource/(x2)bomboShop(674,402).bmp", 674, 402, true, RGB(255, 0, 255));
 	IMAGEMANAGER->addImage("saunaFront", "resource/(x2)saunaFront(896,724).bmp", 896, 724, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("backRockFar1", "resource/(x2)back_rock_far1(82,92).bmp", 82, 92, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("backRockFar2", "resource/(x2)back_rock_far2(96,120).bmp", 96, 120, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("backRockFar3", "resource/(x2)back_rock_far3(90,82).bmp", 90, 82, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("backRock1", "resource/(x2)back_rock1(194,172).bmp", 194, 172, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("backRock2", "resource/(x2)back_rock2(220,244).bmp", 220, 244, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("backRock3", "resource/(x2)back_rock3(170,192).bmp", 170, 192, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("cloud01", "resource/(x2)cloud01(690,602).bmp", 690, 602, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("cloud02", "resource/(x2)cloud02(848,266).bmp", 848, 266, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addImage("cloud03", "resource/(x2)cloud03(380,212).bmp", 380, 212, true, RGB(255, 0, 255));
 	IMAGEMANAGER->addFrameImage("hero", "resource/(x2)hero(3136,2688,14,14).bmp", 3136, 2688, 14, 14, true, RGB(255, 0, 255));
 
 	SCENEMANAGER->addScene("아울보이씬", new owlboyScene);
